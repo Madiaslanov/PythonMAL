0ae78e204f7141e754af7d7b282f812ca264eb7b
Madi <asmadi.rus@mail.ru>
2024-02-16 23:53:37 +0600

    готово

--- lab4/json.py
+++ lab4/json.py
@@ -1,9 +1,9 @@
 import json
 print('Interface Status')
 print('================================================================================')
-print('DN                                  x.                Description           Speed    MTU  ')
+print('DN                                                 Description           Speed    MTU  ')
 print('-------------------------------------------------- --------------------  ------  ------')
-with open('json_sample.json','r') as file:
-    save = json.load(file)
-    for gg in save['imdata']:
-        print(gg["l1PhysIf"]["attributes"]["dn"],'                              ', gg["l1PhysIf"]["attributes"]["fecMode"],'   ',gg["l1PhysIf"]["attributes"]["mtu"])
+with open('json_sample.json', 'r') as file:
+        g = json.load(file)
+        for gg in g['imdata']:
+                print(gg["l1PhysIf"]['attributes']['dn'], '                             ', gg["l1PhysIf"]['attributes']['dn'], '', gg["l1PhysIf"]['attributes']['dn'])
